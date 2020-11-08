1528c0370bcaa63d933779b09646b44b4eaffb56
Steve Karuma <stevekaruma@gmail.com>
2020-11-08 22:04:54 +0300

    Fixed cart error issues

--- src/app/cart/components/payment.jsx
+++ src/app/cart/components/payment.jsx
@@ -1,63 +1,78 @@
-import React from 'react'
-import Api from '../../config/api'
+import React from "react";
+import Api from "../../config/api";
 
-const Payment = ({posta}) => {
+const Payment = ({ posta }) => {
   const handlePayOrder = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-    
-    // document.getElementById('podDiv').style.pointerEvents = 'none'
-    document.getElementById('payBtn').disabled = 'disabled'
-    document.getElementById('payBtn').innerText = 'Confirming Order...'
+    e.preventDefault();
+    e.stopPropagation();
 
-    const user = JSON.parse(localStorage.getItem("user"))
-    const context = JSON.parse(localStorage.getItem("Cart"))
+    // document.getElementById('podDiv').style.pointerEvents = 'none'
+    document.getElementById("payBtn").disabled = "disabled";
+    document.getElementById("payBtn").innerText = "Confirming Order...";
+
+    const user = JSON.parse(localStorage.getItem("user"));
+    const context = JSON.parse(localStorage.getItem("Cart"));
     var cart = [...context];
 
     var order = {
       user: user.id,
-      delivery: posta
-    }
+      delivery: posta,
+    };
 
-    Api.orders.post(order)
-    .then(res=>{
-      var data = res.data
-      for(var i=0; i<cart.length; i++){
-        var item = {
-          quantity: cart[i].quantity,
-          order: data.id,
-          product: cart[i].id,
-          delivery_fee: cart[i].weight < 5 ? (cart[i].quantity*280) : ((((cart[i].weight - 5) * 30)*cart[i].quantity) + (cart[i].quantity*280)),
-          buying_price: cart[i].price
-        }
-        Api.orderitems.post(item)
-        .then(res => {
-          console.log(res.data)
+    if (cart.length >= 1) {
+      Api.orders
+        .post(order)
+        .then((res) => {
+          var data = res.data;
+          for (var i = 0; i < cart.length; i++) {
+            var item = {
+              quantity: cart[i].quantity,
+              order: data.id,
+              product: cart[i].id,
+              delivery_fee:
+                cart[i].weight < 5
+                  ? cart[i].quantity * 280
+                  : (cart[i].weight - 5) * 30 * cart[i].quantity +
+                    cart[i].quantity * 280,
+              buying_price: cart[i].price,
+            };
+            Api.orderitems.post(item).then((res) => {
+              console.log(res.data);
+            });
+          }
+          Api.sendemail.post({ orderid: res.data.id }).then(() => {});
+          document.getElementById("payBtn").innerText = "Order Succesful!";
+          setTimeout(() => {
+            localStorage.removeItem("Cart");
+            window.location.href = "/order-successful/";
+          }, 5000);
         })
-      }
-    })
-    
-    document.getElementById('payBtn').innerText = 'Order Succesful!'
-
-    setTimeout(() => {
-      localStorage.removeItem('Cart')
-      window.location.href = "/order-successful/"
-    }, 3000)
-  }
+        .catch((error) => {
+          document.getElementById("payBtn").innerText = "Order Unsuccesful!";
+          document.getElementById("payBtn").disabled = "";
+          console.log(error);
+        });
+    } else {
+      console.log("Cart is empty!");
+    }
+  };
 
   return (
     <div className="checkout-form">
-
-    <form className="form" onSubmit={handlePayOrder}>
+      <form className="form" onSubmit={handlePayOrder}>
         <h2 className="playfair-m align-center">Payment</h2>
         <p className="lato-sm align-center mg-v-10">
-          The amount is payable through M-PESA PAYBILL. <br/>
-          <strong>Business No. - 400222 <br/> Acc. No. - 450673</strong>
-          </p>
-        <button className="btn btn-full mg-v-50" id="payBtn">Complete Order</button>
+          The amount is payable through M-PESA PAYBILL. <br />
+          <strong>
+            Business No. - 400222 <br /> Acc. No. - 450673
+          </strong>
+        </p>
+        <button className="btn btn-full mg-v-50" id="payBtn">
+          Complete Order
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Payment
+export default Payment;
--- src/app/config/api.js
+++ src/app/config/api.js
@@ -1,48 +1,46 @@
-import Call, {Img} from './axios'
+import Call, { Img } from "./axios";
 
 const Api = () => {
-  
-  var setUrl = (path) => { 
-    return new Call(path)
-  }
+  var setUrl = (path) => {
+    return new Call(path);
+  };
 
-  var setImgUrl = (path) =>{
-    return new Img(path)
-  }
+  var setImgUrl = (path) => {
+    return new Img(path);
+  };
 
-  return (
-    {
-      server_url: setUrl(),
-      auth: setUrl('api/token/') ,
-      refresh: setUrl('api/token/refresh/') ,
-      reset: setUrl('api/password_reset/'),
-      confirmreset: setUrl('api/password_reset/confirm/'),
-      categories: setUrl('categories/') ,
-      subcategories: setUrl('subcategories/') ,
-      productclass: setUrl('productclass/') ,
-      catalog: setUrl('catalog/'),
-      payments: setUrl('payments/payments/'),
-      notifications: setUrl('payments/notifications/'),
-      me: setUrl('users/usersList/me/') ,
-      users: setUrl('users/usersList/') ,
-      userdetails: setUrl('users/userDetails/') ,
-      orders: setUrl('orders/list/'),
-      orderitems: setUrl('orders/orderItems/') ,
-      orderstats: setUrl('orders/stats/') ,
-      orderstatus: setUrl('orders/status/') ,
-      itemsstats: setUrl('orders/itemssold/'),
-      clients: setUrl('clients/clients/') ,
-      clientscats: setUrl('clients/clientsCategory'),
-      postas: setUrl('orders/postas/'),
-      images_url: setUrl('images/') ,
-      images: setImgUrl(''),
-      news: setUrl('posts/news/') ,
-      visitors: setUrl('users/visitors/') ,
-      visitorstats: setUrl('users/visitorStats/'),
-      events: setUrl('posts/events/'),
-      about: setUrl('clients/about/')
-    }
-  )
-}
+  return {
+    server_url: setUrl(),
+    auth: setUrl("api/token/"),
+    refresh: setUrl("api/token/refresh/"),
+    reset: setUrl("api/password_reset/"),
+    confirmreset: setUrl("api/password_reset/confirm/"),
+    categories: setUrl("categories/"),
+    subcategories: setUrl("subcategories/"),
+    productclass: setUrl("productclass/"),
+    catalog: setUrl("catalog/"),
+    payments: setUrl("payments/payments/"),
+    notifications: setUrl("payments/notifications/"),
+    me: setUrl("users/usersList/me/"),
+    users: setUrl("users/usersList/"),
+    userdetails: setUrl("users/userDetails/"),
+    orders: setUrl("orders/list/"),
+    orderitems: setUrl("orders/orderItems/"),
+    orderstats: setUrl("orders/stats/"),
+    orderstatus: setUrl("orders/status/"),
+    itemsstats: setUrl("orders/itemssold/"),
+    clients: setUrl("clients/clients/"),
+    clientscats: setUrl("clients/clientsCategory"),
+    postas: setUrl("orders/postas/"),
+    images_url: setUrl("images/"),
+    images: setImgUrl(""),
+    news: setUrl("posts/news/"),
+    visitors: setUrl("users/visitors/"),
+    visitorstats: setUrl("users/visitorStats/"),
+    events: setUrl("posts/events/"),
+    about: setUrl("clients/about/"),
+    sendemail: setUrl("orders/sendEmail/"),
+  };
+};
 
-export default Api()
+export default Api();
